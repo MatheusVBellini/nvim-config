0944765e4318c43c80a224ece4877813870e441b
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-12-23 19:42:32 -0300

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+-- git status line
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 
+    'nvim-tree/nvim-web-devicons' 
+  },
+
+  config = function ()
+    require('lualine').setup({})
+  end
+
+}
