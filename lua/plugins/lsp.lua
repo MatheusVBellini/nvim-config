a4a4dffd9d18a642505d95cfd0cbe67a14048761
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-12-26 10:19:39 -0300

    auto-completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,14 +2,14 @@
 local servers = {"bashls", "clangd", "cmake", "jsonls", "jdtls", "lua_ls", "marksman",  "jedi_language_server"}
 
 return {
-  { -- [[ language server fetching ]]
+  { -- [[ language server fetcher ]]
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end
   },
 
-  {
+  { -- [[ language server fetching ]]
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
@@ -18,12 +18,67 @@ return {
     end
   },
 
+  { -- [[ auto-completion ]]
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip"
+    },
+    config = function()
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+          ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+          -- C-b (back) C-f (forward) for snippet placeholder navigation.
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        },
+      })
+    end
+  },
+
   { -- [[ language server activation ]]
     "neovim/nvim-lspconfig",
+    dependencies = "hrsh7th/cmp-nvim-lsp",
     config = function()
       local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({})
+        lspconfig[lsp].setup({
+          capabilities = capabilities,
+        })
       end
 
       -- special keybindings
