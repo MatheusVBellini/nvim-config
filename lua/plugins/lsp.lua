9e0d2d59c399834fd66261a1dd6c977a8a33238b
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-12-26 09:48:42 -0300

    better code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,8 @@
 -- language servers
+local servers = {"bashls", "clangd", "cmake", "jsonls", "jdtls", "lua_ls", "marksman",  "jedi_language_server"}
+
 return {
-  {
+  { -- [[ language server fetching ]]
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
@@ -10,27 +12,19 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      -- fetch language servers
-      local servers = {"bashls", "clangd", "cmake", "jsonls", "jdtls", "lua_ls", "marksman",  "jedi_language_server"}
       require("mason-lspconfig").setup({
         ensure_installed = servers
       })
     end
   },
 
-  {
+  { -- [[ language server activation ]]
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      -- activate language servers
-      lspconfig.bashls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.cmake.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.jdtls.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.jedi_language_server.setup({})
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({})
+      end
 
       -- special keybindings
       vim.keymap.set('n', '<leader>ch', vim.lsp.buf.hover, {}) -- show code documentation <ch -> code + hover>
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,12 @@
+-- languages for which to acquire syntax identifier
+local languages = {"lua", "vim", "python", "c", "cpp"}
+
 -- tree syntax identifier
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
   config = function()
-    local languages = {"lua", "vim", "python", "c", "cpp"}
     local configs = require("nvim-treesitter.configs")
     configs.setup({
       ensure_installed = languages,
